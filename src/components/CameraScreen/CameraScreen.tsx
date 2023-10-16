aa464caf3363dc648f72a099bfdd24b063975942
Fabio Di Crescenzo <fabio.dicrescenzo@neosyn.it>
2023-10-16 18:24:04 +0200

    RNC-5 WIP: minor optimization to components/CameraScreen and creation of useIsForeground hook

--- src/components/CameraScreen/CameraScreen.tsx
+++ src/components/CameraScreen/CameraScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {Text, View} from 'react-native';
 
 import {
@@ -7,16 +7,30 @@ import {
   useCameraPermission,
 } from 'react-native-vision-camera';
 
+import {useIsForeground} from '@hooks/useIsForeground';
+
 const CameraScreen = () => {
+  const camera = useRef<Camera>(null);
   const [isCameraInitialized, setCameraInitialized] = useState(false);
-  const device = useCameraDevice('back');
-  const {requestPermission} = useCameraPermission();
+  const [_hasMicrophonePermission, setHasMicrophonePermission] =
+    useState(false);
+  const [cameraPosition, _setCameraPosition] = useState<'front' | 'back'>(
+    'back',
+  );
+
+  // check if camera page is active
+  const isFocused = true; // harcoded due to lack of a screen context (Should evaluate from @react-navigation/core's useIsFocused())
+  const isForeground = useIsForeground();
+  const isActive = isForeground && isFocused;
+
+  const device = useCameraDevice(cameraPosition);
+  const shouldShowCamera = isCameraInitialized && device;
+
+  const {hasPermission, requestPermission} = useCameraPermission();
 
   const initializeCamera = async function () {
     try {
-      await requestPermission();
-      await Camera.getCameraPermissionStatus();
-      await Camera.getMicrophonePermissionStatus();
+      !hasPermission && (await requestPermission());
       setCameraInitialized(true);
     } catch (error) {
       console.error('Error initializing camera:', error);
@@ -25,13 +39,23 @@ const CameraScreen = () => {
 
   useEffect(() => {
     initializeCamera();
+    Camera.getMicrophonePermissionStatus().then(status =>
+      setHasMicrophonePermission(status === 'granted'),
+    );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <View className="flex-1">
-      {isCameraInitialized ? (
-        <Camera className="flex-1" isActive={true} device={device!} />
+    <View className="flex-1 bg-[black]">
+      {shouldShowCamera ? (
+        <Camera
+          ref={camera}
+          className="flex-1"
+          isActive={isActive}
+          device={device}
+          video={true}
+          audio={true}
+        />
       ) : (
         <Text>Loading camera...</Text>
       )}
---
+++ src/components/CameraScreen/CaptureButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const CaptureButton = (_props: Props) => {
+  return <div>CaptureButton</div>;
+};
+
+export default CaptureButton;
