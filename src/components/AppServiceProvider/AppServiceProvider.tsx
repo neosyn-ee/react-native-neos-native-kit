a4f2bd08d95b486e367a6eac332f6f7e26ff82d0
Fabio Di Crescenzo <fabio.dicrescenzo@neosyn.it>
2023-10-23 11:24:21 +0200

    RNC-5 WIP: removes GestureHandlerRootView from the AppSeviceProvider component

--- src/components/AppServiceProvider/AppServiceProvider.tsx
+++ src/components/AppServiceProvider/AppServiceProvider.tsx
@@ -1,14 +1,13 @@
 import React, {ReactNode} from 'react';
 
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {SingletonHooksContainer} from 'react-singleton-hook';
 
 const AppServiceProvider = ({children}: {children?: ReactNode}) => {
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
+    <>
       <SingletonHooksContainer />
       {children}
-    </GestureHandlerRootView>
+    </>
   );
 };
 
