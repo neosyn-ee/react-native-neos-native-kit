e9b98e626cdceb7f9825608aa957e6872d71f975
Fabio Di Crescenzo <fabio.dicrescenzo@neosyn.it>
2023-10-26 11:07:07 +0200

    RNC-5 WIP: adds close and save button (only UI) to MediaScreen and fixes its size adabtability

--- src/components/MediaScreen/MediaScreen.tsx
+++ src/components/MediaScreen/MediaScreen.tsx
@@ -1,32 +1,58 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Image, StyleSheet} from 'react-native';
 
+import {IconButton} from 'react-native-paper';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 import {VideoPlayer} from '@components/VideoPlayer';
-import {SCREEN_WIDTH} from '@utils/constants';
+import {SAFE_AREA_PADDING} from '@utils/constants';
 
 import {MediaScreenProps} from './MediaScreen.types';
 
 const MediaScreen = ({media, type}: MediaScreenProps) => {
-  // NOTE: due to the orientation-related bug, the width and height properties will be used in inverted mode
-  const ratio = 'width' in media ? SCREEN_WIDTH / media.height : undefined; // here media.height is the actual picture's width
-  const mediaHasHeight = 'height' in media && ratio;
+  const {path} = media;
 
   return (
-    <SafeAreaView className="flex-1 bg-[#000000]">
+    <SafeAreaView className="flex-1 bg-[#FFFFFF]">
       {type === 'photo' && (
         <Image
-          width={SCREEN_WIDTH}
-          height={mediaHasHeight ? media.width * ratio : undefined} // here media.width is the actual picture's height
-          source={{uri: `file://${media.path}`}}
+          className="flex-1"
+          source={{uri: `file://${path}`}}
+          resizeMode="cover"
         />
       )}
       {type === 'video' && (
-        <VideoPlayer source={{uri: `file://${media.path}`}} height="100%" />
+        <VideoPlayer source={{uri: `file://${path}`}} height="100%" />
       )}
+      <IconButton
+        style={styles.closeButton}
+        icon="close"
+        iconColor="white"
+        size={30}
+        onPress={() => console.log('close button pressed')}
+      />
+      <IconButton
+        style={styles.saveButton}
+        icon="folder-download"
+        iconColor="white"
+        size={30}
+        onPress={() => console.log('save button pressed')}
+      />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  closeButton: {
+    position: 'absolute',
+    top: SAFE_AREA_PADDING.paddingTop,
+    left: SAFE_AREA_PADDING.paddingLeft,
+  },
+  saveButton: {
+    position: 'absolute',
+    bottom: SAFE_AREA_PADDING.paddingBottom,
+    left: SAFE_AREA_PADDING.paddingLeft,
+  },
+});
 
 export default MediaScreen;
