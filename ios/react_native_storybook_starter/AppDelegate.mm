0ff105b3d919494a8bca80c4123e65f7df99277f
Rossella Mascia <rossella.mascia@neosyn.it>
2024-06-10 17:09:50 +0200

    feat: upgrade react native 0.73.8

--- ios/react_native_storybook_starter/AppDelegate.mm
+++ ios/react_native_storybook_starter/AppDelegate.mm
@@ -2,8 +2,6 @@
 
 #import <React/RCTBundleURLProvider.h>
 
-#import "RNSStorybookDevMenuModule.h"
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -18,22 +16,16 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
+  return [self getBundleURL];
+}
+
+- (NSURL *)getBundleURL
+{
 #if DEBUG
-  BOOL isStorybookEnabled = RNSStorybookDevMenuIsStorybookEnabled();
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:isStorybookEnabled ? @"index.storybook" :  @"index"];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
-}
-
-/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
-///
-/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
-/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
-/// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
-- (BOOL)concurrentRootEnabled
-{
-  return true;
 }
 
 @end
